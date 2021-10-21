07c2471007103ab7782d3c8e83b0b17fce804c0d
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-21 11:10:54 -0500

    adds comments

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,8 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// PUT route to reset all the isPurchased properties to false
+
+// PUT route to remove a specific item with an id
 
 module.exports = router;
