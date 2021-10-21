f1d36de63cc0e81e04022b753e1a369306ffbfd5
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-21 11:35:32 -0500

    PUT /groceries/:id resets isPurchased on single item with id

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
@@ -11,6 +12,7 @@ router.put(`/`, (req, res) => {
     UPDATE "groceries"
     SET "isPurchased" = false;
   `;
+
   // run the query
   pool
     .query(queryText)
@@ -24,5 +26,26 @@ router.put(`/`, (req, res) => {
 });
 
 // PUT route to set isPurchased as true on a specific item with an id
+router.put(`/:id`, (req, res) => {
+  // build the sql query
+  let queryText = `
+    UPDATE "groceries"
+    SET "isPurchased" = true
+    WHERE "id" = $1;
+  `;
+
+  // sanitize the inputs
+  let values = [req.params.id];
+
+  pool
+    .query(queryText, values)
+    .then((response) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(`There was an error communicating with PostgreSQL`, err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
