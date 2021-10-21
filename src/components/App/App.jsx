f21dafa8a297c8c30cefd3b106e2fe185cc2305c
Julia Bayless <julia.allistair@gmail.com>
2021-10-21 14:59:56 -0500

    completed GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Header from '../Header/Header.jsx';
 import './App.css';
 
 function App() {
+  let [groceryList, setGroceryList] = useState([]);
+
+  //when the page opens, you will see...
+  useEffect(() => {
+    fetchGroceries()
+  }, []);
+
+//fetch those groceries from the DB
+  const fetchGroceries = () => {
+    axios.get({
+      method: 'GET',
+      url: '/groceries'
+    })
+    .then(response => {
+      console.log('GET:', response.data);
+      setGroceryList(response.data) 
+  })
+  .catch(error => {
+      console.log('ERROR IN GET', error);
+  })
+  }
+
+
+
+
   return (
     <div className="App">
       <Header />
