c2fdbf2efec3d27890b9ef2494d45a5bed8f0914
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-21 11:30:17 -0500

    PUT /groceries resets all items to isPurchased = false

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,7 +5,24 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 // PUT route to reset all the isPurchased properties to false
+router.put(`/`, (req, res) => {
+  // build the sql query
+  let queryText = `
+    UPDATE "groceries"
+    SET "isPurchased" = false;
+  `;
+  // run the query
+  pool
+    .query(queryText)
+    .then((response) => {
+      res.sendStatus(200); // let the client know the request succeeded
+    })
+    .catch((err) => {
+      console.log(`There was an error communicating with PostgreSQL`, err);
+      res.sendStatus(500);
+    });
+});
 
-// PUT route to remove a specific item with an id
+// PUT route to set isPurchased as true on a specific item with an id
 
 module.exports = router;
