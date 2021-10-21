f955397f88f94122cd2e1faab21aed7bc02ecbb0
Hamsa Alimci <alimici2016@gmail.com>
2021-10-21 14:30:36 -0500

    GET route operational

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,21 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.get('/', (req, res) => {
+    const sqlText = `
+    SELECT * FROM "groceries" ORDER BY "name" DESC;
+    `;
+    pool.query(sqlText)
+    .then((result) =>{
+        console.log('The GET is operational', result)
+        res.send(result.rows);
+    }).catch((error) =>{
+        console.log('error in the GET', error)
+        res.sendStatus(500)
+    })
+})
+
+
+
 
 module.exports = router;
