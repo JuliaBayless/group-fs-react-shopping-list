2bc72119a1720b3498e634aa1fda74c93ba3dbe9
Julia Bayless <julia.allistair@gmail.com>
2021-10-21 11:34:41 -0500

    Delete grocery list '/' working with postman

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,24 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+
+router.delete('/', (req, res) => {
+
+    const queryText = `
+        DELETE from "groceries"
+    `
+
+    pool.query(queryText)
+        .then(result => {
+            res.sendStatus(204)
+        })
+        .catch((error => {
+            console.log('ERROR in delete', error);
+            res.sendStatus(500);
+        }))
+})//end router.delete('/')
+
+
+router.delete('/:id')
+
 module.exports = router;
