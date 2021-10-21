61d4788a98f6dbfde0445b22049dfbe997cc3308
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-21 15:00:12 -0500

    basic layout and functions

--- src/components/GroceryItem/GroceryItem.jsx
+++ src/components/GroceryItem/GroceryItem.jsx
@@ -1,12 +1,57 @@
 // This component is a single grocery item that is displayed within GroceryList
+import axios from 'axios';
 
-// The list item looks like this in layout:
+function GroceryList({ fetchGroceries, groceryItem }) {
+  const buyItem = () => {
+    console.log(
+      `In buyItem, about to do a put with grocery item id`,
+      groceryItem.id
+    );
+    axios
+      .put(`/${groceryItem.id}`)
+      .then((response) => {
+        // refresh the DOM
+        fetchGroceries();
+      })
+      .catch((err) => {
+        console.log(
+          `There was an error updating the item on the database:`,
+          err
+        );
+      });
+  };
 
-// <div>
-//    <p>item.name</p>
-//    <p>{item.quantity} {item.unit}</p>
-//  <div>
-//    <button></button>
-//    <button></button>
-//  </div>
-// </div>
+  const removeItem = () => {
+    console.log(
+      `In buyItem, about to do a delete with grocery item id`,
+      groceryItem.id
+    );
+    axios
+      .delete(`/:id`)
+      .then((response) => {
+        // refresh the DOM
+        fetchGroceries();
+      })
+      .catch((err) => {
+        console.log(
+          `There was an error deleting the item from the database:`,
+          err
+        );
+      });
+  };
+
+  return (
+    <div>
+      <p>{groceryItem.name}</p>
+      <p>
+        {groceryItem.quantity} {groceryItem.unit}
+      </p>
+      <div>
+        <button onClick={buyItem}>Buy</button>
+        <button onClick={removeItem}>Remove</button>
+      </div>
+    </div>
+  );
+}
+
+export default GroceryList;
