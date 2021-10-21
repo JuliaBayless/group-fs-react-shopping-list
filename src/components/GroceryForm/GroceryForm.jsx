f495925b2a4da3698cfd54555a5677421abd02ff
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-21 14:32:48 -0500

    adds empty components

---
+++ src/components/GroceryForm/GroceryForm.jsx
@@ -0,0 +1,1 @@
+// This component is a child of App and takes input from the user to create a new item to buy
---
+++ src/components/GroceryList/GroceryList.jsx
@@ -0,0 +1,1 @@
+// This component is a child of App and displays the grocery list items (GroceryItem)
